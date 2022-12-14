5cdfbada7b9af542a002b04fea51e994852a5d6a
Michal Malý <malym@ibt.cas.cz>
2022-12-14 09:48:52 +0100

    Adapt to new .RSCC files schema

--- src/dnatco/rscc.ts
+++ src/dnatco/rscc.ts
@@ -16,8 +16,28 @@ import { isArr, isNum } from '../util/json';
 
 const RsccCache = new Map<string, Rscc.StructureRscc[]>();
 
-function isRsccList(v: any): v is number[] {
-    return isArr(v, isNum);
+function isRsccList(v: any): v is [atomId: number, rscc: number][] {
+    return isArr(v, (x: unknown): x is [atomId: number, rscc: number] => {
+        if (!isArr(x, isNum))
+            return false;
+        return x.length === 2;
+    });
+}
+
+// TODO: Remove the slow path warning
+let slowPath = 0;
+function findRscc(atomId: number, list: [atomId: number, rscc: number][], startIdx = 0): { rscc: number, idx: number }|undefined {
+    for (let idx = startIdx; idx < list.length; idx++) {
+        const v = list[idx];
+        if (v[0] === atomId)
+            return { rscc: v[1], idx };
+    }
+    const idx = list.findIndex(x => x[0] === atomId);
+
+    console.warn(`RSCC lookup slow path when looking for atomId ${atomId} from index ${startIdx}, total hits ${++slowPath}`);
+
+    return idx >= 0 ? { rscc: list[idx][1], idx } : undefined;
+
 }
 
 export namespace Rscc {
@@ -27,12 +47,13 @@ export namespace Rscc {
         unassigned: StepRscc[];
     };
 
-    function calculateRscc(d: Dnatcofication, modelIdx: number, rsccList: number[]): StructureRscc {
+    function calculateRscc(d: Dnatcofication, modelIdx: number, rsccList: [atomId: number, rscc: number][]): StructureRscc {
         const assigned = new Array<StepRscc>();
         const unassigned = new Array<StepRscc>();
 
         const m = d.data.structures[0].models[modelIdx];
         const steps = StepsMapper.segment(d, modelIdx);
+        let lastAtomIndex = 0;
         for (const step of steps) {
             const chain = m.chains.find(x => x.name === step.chain)!;
             const r1 = chain.residues.find(x => x.num === step.resNo1);
@@ -55,12 +76,24 @@ export namespace Rscc {
                     atomIds.push(atom.id);
             }
 
-            // NO NO NO: This assumes that atomId corresponds to its index in rsccList.
-            // This assumption is very optimistic but good enough for the stopgap code
+            atomIds.sort((a, b) => a - b);
+            if (atomIds.length === 0)
+                continue;
+
             let harmAvgEstimate = 0; // Estimate because we shift the rscc into the [0.0, 2.0] band and unshift the result back
             let counted = 0;
+
+            let found = findRscc(atomIds[0], rsccList, lastAtomIndex);
+            let rollbackAtomIndex = found ? found.idx : lastAtomIndex;
+            lastAtomIndex = rollbackAtomIndex;
+
             for (const id of atomIds) {
-                const rscc = rsccList[id - 1];
+                const found = findRscc(id, rsccList, lastAtomIndex);
+                if (!found)
+                    continue;
+                lastAtomIndex = found.idx;
+
+                const rscc = found.rscc;
                 if (rscc <= -1.0)
                     continue;
 
@@ -73,6 +106,11 @@ export namespace Rscc {
                 unassigned.push({ stepId: step.id, rmsd: step.rmsd, hRscc: harmAvgEstimate });
             else
                 assigned.push({ stepId: step.id, rmsd: step.rmsd, hRscc: harmAvgEstimate });
+
+            // Go back to this index because we will very likely need to start the search
+            // from the top of what is now the second residue in the current step but will
+            // be the first residue of the next step.
+            lastAtomIndex = rollbackAtomIndex;
         }
 
         return { assigned, unassigned };
@@ -93,6 +131,8 @@ export namespace Rscc {
             const rsccList = await req.json();
             if (!isRsccList(rsccList))
                 return ErrorResult('Invalid data');
+
+            rsccList.sort((a, b) => a[0] - b[0]);
 
             const res = calculateRscc(d, modelIdx, rsccList);
 
