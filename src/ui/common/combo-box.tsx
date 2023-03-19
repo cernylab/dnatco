5df3805c033f50f8e3f827b989d5a0274790560a
Michal Malý <malym@ibt.cas.cz>
2023-03-19 22:03:56 +0100

    Make ComboBoxes change value when the mouse wheel is rolled with the cursor hovering above them

--- src/ui/common/combo-box.tsx
+++ src/ui/common/combo-box.tsx
@@ -34,6 +34,23 @@ export class ComboBox extends React.Component<ComboBox.Props> {
                     onChange={e => this.props.onChange(e.currentTarget.value)}
                     style={this.props.innerStyle}
                     disabled={this.props.disabled}
+                    onWheel={(ev) => {
+                        const numOpts = this.props.options.length;
+                        if (numOpts < 1 || this.props.disabled)
+                            return;
+
+                        const y = ev.deltaY;
+                        const direction = y > 0 ? 'down' : y < 0 ? 'up' : 'none';
+
+                        const idx = this.props.options.findIndex(x => x.value === this.props.value);
+                        if (idx < 0)
+                            return; // Should never happen
+
+                        if (direction === 'down' && idx < numOpts - 1)
+                            this.props.onChange(this.props.options[idx + 1].value);
+                        else if (direction === 'up' && idx > 0)
+                            this.props.onChange(this.props.options[idx - 1].value);
+                    }}
                 >
                     {this.props.options.map(o => {
                         return (
