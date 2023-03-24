e67c0d1d688a0a68659e2b5039c7353441b316d7
Michal Malý <malym@ibt.cas.cz>
2023-03-24 10:52:09 +0100

    Pad out the flex with of the last stats block to always get the total width of 100

--- src/ui/dnatco/stats-bar.tsx
+++ src/ui/dnatco/stats-bar.tsx
@@ -4,15 +4,20 @@ export function StatsBar(props: { counts: number[], colors: string[] }) {
     const sum = props.counts.reduce((p, c) => p + c, 0);
 
     const blocks = new Array<JSX.Element>();
-    for (let idx = 0; idx < props.counts.length; idx++) {
+    let accum = 0;
+    for (let idx = 0; idx < props.counts.length - 1; idx++) {
         const n = props.counts[idx];
         if (n === 0)
             continue;
 
         const w = Math.round(100 * n / sum);
-        if (w > 0)
+        if (w > 0) {
             blocks.push(<div style={{ flex: w, backgroundColor: props.colors[idx] }} key={idx} />);
+            accum += w;
+        }
     }
+
+    blocks.push(<div style={{ flex: (100 - accum), backgroundColor: props.colors[props.colors.length - 1] }} key={props.counts.length - 1} />);
 
     return <div className='rdo-hflex' style={{ width: '100%', height: '100%' }}>{blocks}</div>;
 }
