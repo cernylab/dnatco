bbd99e6c7f0bcb639e0fc35b5c0c2a583300217b
Michal Malý <malym@ibt.cas.cz>
2023-03-24 11:05:31 +0100

    Remove unused component

--- src/ui/dnatco/common.tsx
+++ src/ui/dnatco/common.tsx
@@ -1,11 +1,8 @@
 import type { StandardLonghandProperties } from 'csstype';
 import React from 'react';
-import { valueToSemaphore } from './util';
 import { IconTextButton } from '../common/push-button';
 import { GlobalConfig } from '../../global-config';
-import { rgbToHex } from '../util';
 import { clamp } from '../../util';
-import { StepRmsdStats as DnatcoStepRmsdStats } from '../../dnatco/dnatcofication';
 import { Step } from '../../dnatco/step';
 
 export namespace Common {
@@ -89,99 +86,5 @@ export namespace DownloadButton {
     export interface Props {
         caption: string;
         onClick: (e: React.MouseEvent) => void;
-    }
-}
-
-export class StepRmsdStats extends React.Component<{ stats: DnatcoStepRmsdStats[] }> {
-    private barRef = React.createRef<HTMLCanvasElement>();
-
-    private drawBar(canvas: HTMLCanvasElement, stats: DnatcoStepRmsdStats[]) {
-        const ctx = canvas.getContext('2d');
-        if (!ctx || stats.length < 3)
-            return;
-
-        const tw = canvas.width;
-        const th = canvas.height;
-        const green = stats[0].rmsdThreshold;
-        const red = stats[stats.length - 2].rmsdThreshold;
-
-        const total = stats.reduce((p, c) => p + c.count, 0);
-
-        let fx = 0;
-        for (let idx = 0; idx < stats.length; idx++) {
-            const s = stats[idx];
-            const thrPrev = stats[idx - 1]?.rmsdThreshold ?? 0;
-            const v = s.rmsdThreshold === -1 ? red + 0.1 : thrPrev + (s.rmsdThreshold - thrPrev) / 2.0;
-
-            const w = Math.round(tw * s.count / total);
-            const rgb = valueToSemaphore(v, green ,red);
-
-            ctx.fillStyle = rgbToHex(rgb);
-            ctx.fillRect(fx, 0, w, th);
-            if (w >= tw)
-                return;
-
-            fx += w;
-        }
-    }
-
-    private tryDrawBar() {
-        const ref = this.barRef.current;
-        if (ref)
-            this.drawBar(ref, this.props.stats);
-    }
-
-    componentDidMount() {
-        this.tryDrawBar();
-    }
-
-    componentDidUpdate() {
-        this.tryDrawBar();
-    }
-
-    render() {
-        const stats = this.props.stats;
-
-        if (stats.length < 2)
-            return void 0;
-
-        const green = stats[0].rmsdThreshold;
-        const red = stats[stats.length - 2].rmsdThreshold;
-        const headers = [<th key={-1} className='rdo-data-table-small'>RMSD{'\u00A0\u212B'}</th>];
-        let idx = 0;
-        for (;idx < stats.length - 1; idx++) {
-            const s = stats[idx];
-            const thrPrev = stats[idx - 1]?.rmsdThreshold ?? 0;
-            const v = s.rmsdThreshold === -1 ? red + 0.1 : thrPrev + (s.rmsdThreshold - thrPrev) / 2.0;
-            headers.push(
-                <th
-                    key={idx}
-                    className='rdo-data-table-small'
-                    style={{ color: rgbToHex(valueToSemaphore(v, green, red)) }}
-                >
-                    {`<\u00A0${s.rmsdThreshold.toFixed(1)}`}
-                </th>
-            );
-        }
-        headers.push(<th key={stats.length - 1} className='rdo-data-table-small' style={{ color: rgbToHex({ r: 255, g: 0, b: 0}) }}>{`>\u00A0${stats[stats.length - 2].rmsdThreshold.toFixed(1)}`}</th>);
-
-        const nums = [
-            <td key={-1} className='rdo-numeric-table-small'></td>,
-            ...stats.map((x, idx) => <td key={idx} className='rdo-numeric-table-small'>{x.count}</td>)
-        ];
-
-        return (
-            <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--v-gap)' }}>
-                <table className='rdo-data-table-small' style={ Common.StyleTableSameColumnWidth }>
-                    <thead>
-                        <tr>{headers}</tr>
-                    </thead>
-                    <tbody>
-                        <tr>{nums}</tr>
-                    </tbody>
-                </table>
-                <canvas width={300} height={1} style={ Common.StyleScoreBar } ref={this.barRef} />
-            </div>
-        );
     }
 }
