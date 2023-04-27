11864ded3dada588410373b95fc9e5e15f3ee9ec
Michal Malý <malym@ibt.cas.cz>
2023-04-27 15:45:32 +0200

    Add compact variant of the navigation bar for smaller screens

--- src/ui/navigation-bar.tsx
+++ src/ui/navigation-bar.tsx
@@ -1,73 +1,219 @@
 import * as React from 'react';
+import * as RDC from 'react-dom/client';
 import { EquiBox } from './common/equibox';
 import { BasePushButton } from './common/push-button';
 import { GlobalConfig }  from '../global-config';
+import '../../../assets/imgs/grid-three-up.svg';
 
-interface TabButtonProps {
+const MinimumWidthForStandardBar = 1000;
+
+function makeMenu(
+    tabs: NavigationBar.Tabs,
+    selectedTab: keyof NavigationBar.Tabs,
+    onTabSwitched: (tk: string) => void,
+    x: number,
+    y: number,
+    onDismissed: () => void,
+) {
+    const tainer = document.createElement('div');
+    document.body.appendChild(tainer);
+
+    const reactRoot = RDC.createRoot(tainer!);
+    reactRoot.render(
+        <Menu
+            tabs={tabs}
+            selectedTab={selectedTab}
+            onTabSwitched={onTabSwitched}
+            onDismissed={onDismissed}
+            x={x} y={y}
+            parentElement={tainer}
+        />
+    );
+}
+
+function makeTabs(tabs: NavigationBar.Tabs, selectedTab: keyof NavigationBar.Tabs, onTabSwitched: (tk: string) => void) {
+    const list = new Array<JSX.Element>();
+
+    for (const tk in tabs) {
+        const tab = tabs[tk];
+        list.push(
+            <TabButton
+                key={tk}
+                icon={tab.icon}
+                caption={tab.caption}
+                onClick={() => onTabSwitched(tk)}
+                selected={selectedTab === tk}
+                enabled={tab.enabled}
+                noCaps={tab.noCaps ?? false}
+            />
+        );
+    }
+
+    return list;
+}
+
+function tabButtonCls(enabled: boolean, selected: boolean) {
+    if (enabled)
+        return `rdo-tab-button rdo-tab-button-enabled ${selected ? 'rdo-tab-button-selected' : ''}`;
+    return 'rdo-tab-button rdo-tab-button-disabled';
+}
+
+function TabButton(props: {
     onClick: () => void;
     caption: string;
     icon: string;
     selected: boolean;
     enabled: boolean;
     noCaps: boolean;
+}) {
+    return (
+        <BasePushButton
+            className={tabButtonCls(props.enabled, props.selected)}
+            classNameDisabled='rdo-tab-button rdo-tab-button-disabled'
+            onClick={props.onClick}
+        >
+            <img
+                className='rdo-tab-button-icon'
+                src={`${GlobalConfig.data().pathPrefix}/${props.icon}`}
+            />
+            <div className={props.noCaps ? 'rdo-tab-button-text-no-caps' : 'rdo-tab-button-text'}>{props.caption}</div>
+        </BasePushButton>
+    );
 }
-class TabButton extends React.Component<TabButtonProps> {
-    private getCls() {
-        if (this.props.enabled)
-            return `rdo-tab-button rdo-tab-button-enabled ${this.props.selected ? 'rdo-tab-button-selected' : ''}`;
-        return 'rdo-tab-button rdo-tab-button-disabled';
-    }
 
-    render() {
-        return (
-            <BasePushButton
-                className={this.getCls()}
-                classNameDisabled='rdo-tab-button rdo-tab-button-disabled'
-                onClick={this.props.onClick}
-            >
+export function Menu(props: {
+    tabs: NavigationBar.Tabs,
+    selectedTab: keyof NavigationBar.Tabs,
+    onTabSwitched: (tk: string) => void,
+    x: number,
+    y: number,
+    onDismissed: () => void,
+    parentElement: HTMLElement,
+}) {
+    const dismisser = () => {
+        document.body.removeChild(props.parentElement);
+        document.body.removeEventListener('click', dismisser);
+        props.onDismissed();
+    };
+
+    React.useEffect(() => {
+        document.body.addEventListener('click', dismisser);
+        return () => document.body.removeEventListener('click', dismisser);
+    });
+
+    return (
+        <div
+            className='rdo-navigation-menu'
+            style={{
+                left: `${props.x}px`,
+                top: `${props.y}px`,
+            }}
+        >
+            {makeTabs(props.tabs, props.selectedTab, props.onTabSwitched)}
+        </div>
+    );
+}
+
+function NavigationBarCompact(props: {
+    tabs: NavigationBar.Tabs;
+    selectedTab: keyof NavigationBar.Tabs;
+    onTabSwitched: (tk: string) => void;
+}) {
+    const [hamburgerOpen, setHamburberOpen] = React.useState(false);
+    const [hamburgerHovered, setHamburberHovered] = React.useState(false);
+    const selected = props.tabs[props.selectedTab]
+
+    return (
+        <div className='rdo-navigation-bar'>
+            <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', gap: 'var(--h-gap)' }}>
+                <div style={{ flex: 1 }} />
                 <img
-                    className='rdo-tab-button-icon'
-                    src={`${GlobalConfig.data().pathPrefix}/${this.props.icon}`}
+                    onMouseEnter={() => setHamburberHovered(true)}
+                    onMouseLeave={() => setHamburberHovered(false)}
+                    className={`rdo-navigation-bar-hamburger-icon ${hamburgerHovered ? 'rdo-navigation-bar-hamburger-icon-active' : ''}`}
+                    src={`${GlobalConfig.data().pathPrefix}/imgs/grid-three-up.svg`}
+                    onClick={(ev) => {
+                        ev.preventDefault();
+                        ev.stopPropagation();
+
+                        if (hamburgerOpen)
+                            return;
+
+                        makeMenu(
+                            props.tabs,
+                            props.selectedTab,
+                            props.onTabSwitched,
+                            ev.clientX,
+                            ev.clientY,
+                            () => setHamburberOpen(false),
+                        );
+                        setHamburberOpen(true);
+                    }}
                 />
-                <div className={this.props.noCaps ? 'rdo-tab-button-text-no-caps' : 'rdo-tab-button-text'}>{this.props.caption}</div>
-            </BasePushButton>
-        );
-    }
+                <div style={{ width: 'min-content' }}>
+                    <TabButton
+                        key={props.selectedTab}
+                        icon={selected.icon}
+                        caption={selected.caption}
+                        onClick={() => {}}
+                        selected={true}
+                        enabled={true}
+                        noCaps={selected.noCaps ?? false}
+                    />
+                </div>
+                <div style={{ flex: 1 }} />
+            </div>
+        </div>
+    );
 }
 
-export class NavigationBar extends React.Component<NavigationBar.Props> {
-    private makeTabs(tabs: NavigationBar.Tabs) {
-        const list = new Array<JSX.Element>();
+function NavigationBarStandard(props: {
+    onTabSwitched: (tk: string) => void;
+    tabs: NavigationBar.Tabs;
+    selectedTab: keyof NavigationBar.Tabs;
+}) {
+    return (
+        <div className='rdo-navigation-bar'>
+            <EquiBox
+                items={makeTabs(props.tabs, props.selectedTab, props.onTabSwitched)}
+                padding={33}
+                orientation='row'
+            />
+        </div>
+    );
+}
 
-        for (const tk in tabs) {
-            const tab = tabs[tk];
-            list.push(
-                <TabButton
-                    key={tk}
-                    icon={tab.icon}
-                    caption={tab.caption}
-                    onClick={() => this.props.onTabSwitched(tk)}
-                    selected={this.props.selectedTab === tk}
-                    enabled={tab.enabled}
-                    noCaps={tab.noCaps ?? false}
-                />
-            );
-        }
+export function NavigationBar(props: {
+    onTabSwitched: (tk: string) => void;
+    tabs: NavigationBar.Tabs;
+    selectedTab: keyof NavigationBar.Tabs;
+}) {
+    const [compact, setCompact] = React.useState(false);
 
-        return list;
-    }
+    React.useEffect(() => {
+        const compactToggler = () => {
+            setCompact(window.innerWidth < MinimumWidthForStandardBar);
+        };
 
-    render() {
-        return (
-            <div className='rdo-navigation-bar'>
-                <EquiBox
-                    items={this.makeTabs(this.props.tabs)}
-                    padding={33}
-                    orientation='row'
-                />
-            </div>
-        );
-    }
+        addEventListener('resize', compactToggler);
+
+        return () => {
+            removeEventListener('resize', compactToggler);
+        };
+    });
+
+    React.useLayoutEffect(() => {
+        setCompact(window.innerWidth < MinimumWidthForStandardBar);
+    });
+
+    return (
+        <div>
+            {compact
+                ? <NavigationBarCompact { ...props } />
+                : <NavigationBarStandard { ...props } />
+            }
+        </div>
+    );
 }
 
 export namespace NavigationBar {
@@ -79,10 +225,4 @@ export namespace NavigationBar {
     };
 
     export type Tabs = Record<string, Tab>;
-
-    export interface Props {
-        onTabSwitched: (tk: string) => void;
-        tabs: Tabs;
-        selectedTab: keyof Tabs;
-    }
 }
