4c84328337f42923b4e8f8224ab9e0fdcfbfde4b
Michal Malý <malym@ibt.cas.cz>
2023-06-06 14:34:57 +0200

    Say "Entity name" instead of "Molecule name" in validation report

--- src/report/content/structure-info.ts
+++ src/report/content/structure-info.ts
@@ -59,7 +59,7 @@ export namespace StructureInfo {
         tbl.addRow([
             NTTable.Cell.lineText('Entity ID', tbl, { font: Tables.HeaderFont }),
             NTTable.Cell.lineText('Entity type', tbl, { font: Tables.HeaderFont }),
-            NTTable.Cell.lineText('Molecule name', tbl, { font: Tables.HeaderFont }),
+            NTTable.Cell.lineText('Entity name', tbl, { font: Tables.HeaderFont }),
             NTTable.Cell.lineText('Count', tbl, { font: Tables.HeaderFont, hAlign: 'right' }),
         ]);
         const { id, pdbx_description, pdbx_number_of_molecules, type, _rowCount } = ctx.dnatcofication.table(Entity);
