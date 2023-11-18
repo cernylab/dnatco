16bc5e7c03fe50921494f9e00bb5dc0f7760d19a
Jiri Cerny <jiri.cerny@ibt.cas.cz>
2023-11-18 13:41:33 +0100

    Corrections of help texts in Annotation

--- src/ui/dnatco/views/annotation/help-annotation.tsx
+++ src/ui/dnatco/views/annotation/help-annotation.tsx
@@ -7,19 +7,21 @@ export class HelpAnnotation extends View {
                 <div>
                     <h2 className='font-din-2014 font-700 text-20px mb-2'>Annotation</h2>
                     <div className='font-din-2014 text-16px mb-2'>
-                        The Annotation page provides users with a concise summary of the analysis results, based on the assignment of NtC classes. The page is organised into three tabs: Assigned NtCs, Structure Info, and Downloads.
+                        The Annotation page provides users with a concise summary of the analysis results, based on the assignment of NtC classes. The page is organised into three tabs: "Assigned NtCs", "Structure Info", and "Downloads".
                     </div>
                     <h3 className='font-din-2014 font-700 text-18px mb-2'>Assigned NtCs tab</h3>
                     <div className='font-din-2014 text-16px mb-2'>
-                        cture and their assigned NtC class and CANA letter. By hovering the mouse over the info button for each dinucleotide step, users can access more detailed information on the 12 parameters defining NtC. The Mol* viewer on the right side of the tab provides a cartoon mode visualisation of the nucleic acid structure, with NtC conformations represented by colour-coded phosphate-centred pyramids. The Mol* viewer and the table are interconnected, so selecting a step in the table highlights the selected step in the Mol* viewer (showing an overlaid reference structure in yellow) and vice versa. For larger structures containing many chains, such as the ribosome, users can list results for only one selected chain, with the Mol* viewer displaying the selected chain only. 
+                        The "Assigned NtCs" tab is divided into three sections: a left sidebar with tabs, a central section featuring a table of results, and a right panel housing a Mol* viewer. The results table showcases all dinucleotide steps within the structure, along with their corresponding NtC class and CANA letter assignments. Hovering the mouse over the information button for each dinucleotide step grants users access to more detailed information about the 12 parameters defining the NtC conformation.
+                        <br/>
+                        The Mol* viewer on the right side of the tab offers a visual representation of the nucleic acid structure in cartoon mode, with NtC conformations represented by colour-coded phosphate-centred pyramids. Importantly, the Mol* viewer and the table are interconnected, meaning that selecting a step in the table will highlight the chosen step in the Mol* viewer (showing an overlaid reference structure in yellow), and vice versa. In the case of larger structures with multiple chains, like the ribosome, users can filter the results to display data for only one selected chain, and the Mol* viewer will accordingly present the selected chain exclusively.
                     </div>
                     <h3 className='font-din-2014 font-700 text-18px mb-2'>Structure Info tab</h3>
                     <div className='font-din-2014 text-16px mb-2'>
-                        The Structure Info tab lists information available from the mmCIF file, such as structure determination method, resolution, PDB deposition date, and literature reference.
+                        The "Structure Info" tab lists information available from the mmCIF file, such as structure determination method, resolution, PDB deposition date, and literature reference.
                     </div>
                     <h3 className='font-din-2014 font-700 text-18px mb-2'>Downloads tab</h3>
                     <div className='font-din-2014 text-16px mb-2'>
-                        The Downloads tab provides links to download the table of assigned NtCs in CSV and JSON formats, as well as an annotated mmCIF file with additional information on the NtC assigned steps.
+                        The "Downloads" tab provides links to download the table of assigned NtCs in CSV and JSON formats, as well as an annotated mmCIF file with additional information on the NtC assigned steps.
                     </div>
                 </div>
             );
