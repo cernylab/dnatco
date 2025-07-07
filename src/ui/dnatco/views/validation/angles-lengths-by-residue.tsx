2817c99829529771363f14707edf4e06595d1be7
Ing Jiří Černý PhD <jiri.cerny@ibt.cas.cz>
2025-07-07 11:38:58 +0200

    Revert "Merge branch 'Lengths&amp;Angles' into 'new-style'"
    This reverts merge request !7

--- src/ui/dnatco/views/validation/angles-lengths-by-residue.tsx
+++ src/ui/dnatco/views/validation/angles-lengths-by-residue.tsx
@@ -89,18 +89,7 @@ const StatsDownloaders = [
   },
 ] as StatsDownloader[];
 
-function reverseTagTriplet(tripletTag: string){
-	const parts = tripletTag.split('^');
-	if (parts.length !== 3){
-	  throw new Error('Invalid tripletTag format: ${tripletTag}');
-	}
-	return [parts[2], parts[1], parts[0]].join('^');
-}
-
 function makeAngleDetails(props: ResidueDetailsProps) {
-  if (!props.residue || !props.residue.bondAngles) {
-    return [];
-  }
   const displayOrder =
     AnglesLengthsDisplayOrder.Angles[props.residue.compound];
 
@@ -110,9 +99,6 @@ function makeAngleDetails(props: ResidueDetailsProps) {
 
     for (let _idx = 0; _idx < props.residue.bondAngles.length; _idx++) {
       if (props.residue.bondAngles[_idx].tag === tripletTag) {
-        idx = _idx;
-        break;
-      } else if (props.residue.bondAngles[_idx].tag === reverseTagTriplet(tripletTag)) {
         idx = _idx;
         break;
       }
@@ -143,10 +129,6 @@ function makeAngleDetails(props: ResidueDetailsProps) {
   return elems;
 }
 
-function reverseTag(pairTag: string){
-	return pairTag.split('^').reverse().join('^');
-}
-
 function makeLengthDetails(props: ResidueDetailsProps) {
   const displayOrder =
     AnglesLengthsDisplayOrder.Lengths[props.residue.compound];
@@ -157,9 +139,6 @@ function makeLengthDetails(props: ResidueDetailsProps) {
 
     for (let _idx = 0; _idx < props.residue.bondLengths.length; _idx++) {
       if (props.residue.bondLengths[_idx].tag === pairTag) {
-        idx = _idx;
-        break;
-      } else if (props.residue.bondLengths[_idx].tag === reverseTag(pairTag)) {
         idx = _idx;
         break;
       }
--- src/ui/dnatco/views/validation/angles-lengths-display-order.ts
+++ src/ui/dnatco/views/validation/angles-lengths-display-order.ts
@@ -16,12 +16,6 @@ const BackboneAnglesOrder = [
 ];
 
 const RiboseRingAnglesOrder = [
-  ...DeoxyRiboseRingAnglesOrder,
-  tripletTag(["C3'", "C2'", "O2'"]),
-  tripletTag(["C1'", "C2'", "O2'"]),
-];
-
-const DeoxyRiboseRingAnglesOrder = [
   tripletTag(["C4'", "O4'", "C1'"]),
   tripletTag(["O4'", "C1'", "C2'"]),
   tripletTag(["C1'", "C2'", "C3'"]),
@@ -32,26 +26,6 @@ const DeoxyRiboseRingAnglesOrder = [
 const AdenineAnglesOrder = [
   ...BackboneAnglesOrder,
   ...RiboseRingAnglesOrder,
-  tripletTag(["O4'", "C1'", "N9"]),
-  tripletTag(["C1'", "N9", "C8"]),
-  tripletTag(["N7", "C8", "N9"]),
-  tripletTag(["C5", "N7", "C8"]),
-  tripletTag(["C4", "C5", "N7"]),
-  tripletTag(["N1", "C6", "C5"]),
-  tripletTag(["N1", "C6", "N6"]),
-  tripletTag(["N6", "C6", "C5"]),
-  tripletTag(["C2", "N1", "C6"]),
-  tripletTag(["N3", "C2", "N1"]),
-  tripletTag(["N3", "C4", "C5"]),
-  tripletTag(["N9", "C4", "C5"]),
-  tripletTag(["C8", "N9", "C4"]),
-  tripletTag(["C1'", "N9", "C4"]),
-  tripletTag(["C2'", "C1'", "N9"]),
-];
-
-const deoxyAdenineAnglesOrder = [
-  ...BackboneAnglesOrder,
-  ...DeoxyRiboseRingAnglesOrder,
   tripletTag(["O4'", "C1'", "N9"]),
   tripletTag(["C1'", "N9", "C8"]),
   tripletTag(["N7", "C8", "N9"]),
@@ -87,50 +61,9 @@ const CytidineAnglesOrder = [
   tripletTag(["C2'", "C1'", "N1"]),
 ];
 
-const deoxyCytidineAnglesOrder = [
-  ...BackboneAnglesOrder,
-  ...DeoxyRiboseRingAnglesOrder,
-  tripletTag(["O4'", "C1'", "N1"]),
-  tripletTag(["C1'", "N1", "C6"]),
-  tripletTag(["C5", "C6", "N1"]),
-  tripletTag(["C4", "C5", "C6"]),
-  tripletTag(["N3", "C4", "C5"]),
-  tripletTag(["N4", "C4", "C5"]),
-  tripletTag(["N3", "C4", "N4"]),
-  tripletTag(["C2", "N3", "C4"]),
-  tripletTag(["N1", "C2", "N3"]),
-  tripletTag(["O2", "C2", "N3"]),
-  tripletTag(["N1", "C2", "O2"]),
-  tripletTag(["C1'", "N1", "C2"]),
-  tripletTag(["C2'", "C1'", "N1"]),
-];
-
-
 const GuanosineAnglesOrder = [
   ...BackboneAnglesOrder,
   ...RiboseRingAnglesOrder,
-  tripletTag(["O4'", "C1'", "N9"]),
-  tripletTag(["C1'", "N9", "C8"]),
-  tripletTag(["N7", "C8", "N9"]),
-  tripletTag(["C5", "N7", "C8"]),
-  tripletTag(["C4", "C5", "N7"]),
-  tripletTag(["N1", "C6", "C5"]),
-  tripletTag(["O6", "C6", "C5"]),
-  tripletTag(["N1", "C6", "O6"]),
-  tripletTag(["C2", "N1", "C6"]),
-  tripletTag(["N3", "C2", "N1"]),
-  tripletTag(["N2", "C2", "N1"]),
-  tripletTag(["N3", "C2", "N2"]),
-  tripletTag(["N3", "C4", "C5"]),
-  tripletTag(["N9", "C4", "C5"]),
-  tripletTag(["C8", "N9", "C4"]),
-  tripletTag(["C1'", "N9", "C4"]),
-  tripletTag(["C2'", "C1'", "N9"]),
-];
-
-const deoxyGuanosineAnglesOrder = [
-  ...BackboneAnglesOrder,
-  ...DeoxyRiboseRingAnglesOrder,
   tripletTag(["O4'", "C1'", "N9"]),
   tripletTag(["C1'", "N9", "C8"]),
   tripletTag(["N7", "C8", "N9"]),
@@ -173,24 +106,6 @@ const ThymineAnglesOrder = [
 const UracilAnglesOrder = [
   ...BackboneAnglesOrder,
   ...RiboseRingAnglesOrder,
-  tripletTag(["O4'", "C1'", "N1"]),
-  tripletTag(["C1'", "N1", "C6"]),
-  tripletTag(["C5", "C6", "N1"]),
-  tripletTag(["C4", "C5", "C6"]),
-  tripletTag(["N3", "C4", "C5"]),
-  tripletTag(["O4", "C4", "C5"]),
-  tripletTag(["N3", "C4", "O4"]),
-  tripletTag(["C2", "N3", "C4"]),
-  tripletTag(["N1", "C2", "N3"]),
-  tripletTag(["O2", "C2", "N3"]),
-  tripletTag(["N1", "C2", "O2"]),
-  tripletTag(["C1'", "N1", "C2"]),
-  tripletTag(["C2'", "C1'", "N1"]),
-];
-
-const deoxyUracilAnglesOrder = [
-  ...BackboneAnglesOrder,
-  ...DeoxyRiboseRingAnglesOrder,
   tripletTag(["O4'", "C1'", "N1"]),
   tripletTag(["C1'", "N1", "C6"]),
   tripletTag(["C5", "C6", "N1"]),
@@ -285,8 +200,6 @@ const UracilLengthsOrder = [
   pairTag(["N1", "C2"]),
 ];
 
-
-
 const ThymineLengthsOrder = [
   ...BackboneLengthsOrder,
   ...RiboseRingLengthsOrder,
@@ -308,11 +221,11 @@ export namespace AnglesLengthsDisplayOrder {
     C: CytidineAnglesOrder,
     G: GuanosineAnglesOrder,
     U: UracilAnglesOrder,
-    DA: deoxyCytidineAnglesOrder,
-    DC: deoxyCytidineAnglesOrder,
-    DG: deoxyGuanosineAnglesOrder,
+    DA: AdenineAnglesOrder,
+    DC: CytidineAnglesOrder,
+    DG: GuanosineAnglesOrder,
     DT: ThymineAnglesOrder,
-    DU: deoxyUracilAnglesOrder,
+    DU: UracilAnglesOrder,
   } as const;
 
   export const Lengths: Record<ElementaryResidue, string[]> = {
