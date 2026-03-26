394049d34a42d6d5208b5e0f8cb40ede3525d925
TallonsCZ <tallons@burningcube.eu>
2026-03-26 10:47:12 +0100

    add alt name for img

--- src/ui/common/side-switching-panel.tsx
+++ src/ui/common/side-switching-panel.tsx
@@ -152,6 +152,7 @@ export function SideSwitchingPanel<K extends string>(props: {
                 >
                     <img
                         className={`rdo-side-switching-panel-hamburger-icon rdo-side-switching-panel-hamburger-icon-${hamburgerHovered ? 'active' : 'inactive'} cursor-pointer`}
+                        alt={"hamburger menu"}
                         src={GridThreeUpImg}
                     />
                 </div>
